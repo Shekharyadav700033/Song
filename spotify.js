04f5d8e72d47e8c29af6bbcc848e31e5fe7d8410
Shekharyadav700033 <shekharyadav700033@gmail.com>
2025-02-02 13:50:18 +0530

    song website
    This is my first website.

---
+++ spotify.js
@@ -0,0 +1,15 @@
+let a = document.getElementById("shreya");
+a.addEventListener('click',function(){
+    a.style.backgroundColor="green";
+    Audio.play();
+});
+
+//shreya
+let buttons = document.getElementById("login");
+buttons.addEventListener('click',function(){
+    buttons.style.backgroundColor="green";
+  
+});
+
+let body = document.querySelectorAll("body");
+
